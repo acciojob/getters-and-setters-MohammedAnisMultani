c27c5fd7acc35457e843bcdd75e34b2a7c548e23
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-02 22:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 //complete this code
-class Person {}
+class Person {
+	  #name;
+        #age;
+        constructor(name,age){
+          this.#name = name
+          this.#age = age
 
-class Student extends Person {}
+        }
+        set age(data){
+          this.#age = data
+        }
 
-class Teacher extends Person {}
+        get name(){
+          return this.#name
+        }
+}
+
+class Student extends Person {
+	 constructor(name,age){
+          super(name,age)
+        }
+        study(){
+          console.log(`${this.name} is studying`)
+        }
+}
+
+class Teacher extends Person {
+	 constructor(name,age){
+          super(name,age)
+
+        }
+        teach(){
+          console.log(`${this.name} is teaching`)
+        }
+}
+ const person = new Person("Anis",24)
+      console.log(person.name)
+
+      person.age = 30
+      console.log(person.age)
+
+      const student = new Student("shamim",27)
+      student.study()
+      
+      const teacher = new Teacher("bob",40)
+      teacher.teach()
 
 // Do not change the code below this line
 window.Person = Person;
